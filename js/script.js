e9c711a5c849c03ff9b81b75a74aca28d9e4bd07
Antonio Sitoe <72309855+Antonio-Sitoe@users.noreply.github.com>
2021-04-29 22:42:02 +0200

    NEW

--- js/script.js
+++ js/script.js
@@ -1,18 +1,14 @@
 import initAnimaScroll from './Modules/animaScroll.js';
 import initModal from './Modules/modal.js';
 import initEventGalery from './Modules/eventGalery.js';
-import initAcordion from './Modules/acordion.js';
-import outsideClick from './Modules/outsideclick.js';
-import tables from './Modules/tabless.js'
-import menuMobile from './Modules/menuMobile.js'
+import menuMobile from './Modules/menuMobile.js';
+import animaNumeros from './Modules/animaNumeros.js';
 
 initAnimaScroll();
 initModal();
 initEventGalery();
-initAcordion();
-tables();
 menuMobile();
-outsideClick();
+animaNumeros();
 
 
 
