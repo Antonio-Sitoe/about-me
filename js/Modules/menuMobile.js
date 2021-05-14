2d206125547918032b83520a3596211b29cbe4d9
antoniositoehl@gmail.com <72309855+Antonio-Sitoe@users.noreply.github.com>
2021-05-14 20:58:05 +0200

    NEW COMIT

--- js/Modules/menuMobile.js
+++ js/Modules/menuMobile.js
@@ -1,22 +1,23 @@
-import outsideClick from './outsideclick.js';
-
 export default function menuMobile() {
 
+    const menuButton = document.querySelector('[data-menu="button"]')
+    const menuLista = document.querySelector('[data-menu="lista"]')
+
+    const eventos = ['touchstart', 'click']
+    function menuOpen() {
+        menuLista.classList.toggle('ative')
+        window.addEventListener('click', handleEventCLick)
+    }
+
+    eventos.forEach((event) => {
+        menuButton.addEventListener(event, menuOpen)
+    })
+
+    function handleEventCLick(e) {
+        const lis = menuLista.querySelectorAll('li');
+        if (e.target !== menuLista && e.target !== menuButton && e.target !== lis[0] && e.target !== lis[1] && e.target !== lis[2]) {
+            menuLista.classList.remove('ative')
+        }
+    }
+
 }
-
-const menuButton = document.querySelector('[data-menu="button"]')
-const menuLista = document.querySelector('[data-menu="lista"]')
-
-const eventos = ['touchstart', 'click']
-
-
-
-function menuOpen() {
-    menuLista.classList.add('ative')
-}
-
-
-
-eventos.forEach((event) => {
-    menuButton.addEventListener(event, menuOpen)
-})
